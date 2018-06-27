fb24f85fc0c989e431903e8281664058a75dcf39
Havit_R <havitchoirul.rovik@mitrais.com>
2018-06-27 21:37:37 +0700

    Implement Get for Job Post Status

---
+++ api/controllers/JobPostStatus.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const JobPostStatus = require('../models').JobPostStatus;
+
+// Exports all the functions to perform on the db
+module.exports = {
+  getJobPostStatus
+};
+
+// GET /jobpoststatus operationId
+function getJobPostStatus(req, res) {
+  JobPostStatus.findAll()
+    .then((result) => {
+      const jobPostStatus = result.map((node) => {
+        const {
+          id,
+          description
+        } = node.get({
+          plain: true
+        });
+
+        return {
+          id,
+          description,
+        };
+      });
+
+      res.json({
+        jobPostStatus
+      });
+    })
+    .catch((err) => {
+      res.status(204).send(err);
+    });
+}
